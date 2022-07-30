7b2d0aee469fac1e50ccf4f3670c49fbe682fe90
jyothu <arunjyothishvikku@gmail.com>
2022-07-30 17:40:05 +0530

    onebutton code rev1

--- src/main/onebutton/onebutton.ino
+++ src/main/onebutton/onebutton.ino
@@ -11,9 +11,11 @@ int ecg_signal  = 0 ;
 unsigned long int endtime =0; 
 unsigned long int starttime = 0;
 String Ecg_data ="data: ";
-
+bool flag = false;
 const float cutoff_freq   = 17.0;  //Cutoff frequency in Hz
 const float sampling_time = 0.007; //Sampling time in seconds.
+
+//int count =0 ;/
 IIR::ORDER  order  = IIR::ORDER::OD4; // Order (OD1 to OD4)
 
 // Low-pass filter
@@ -31,8 +33,10 @@ class Button{
 		}
 
 		void Clicked(){
-			int init_delay = 1000;
-			int end_delay = 5000;
+			Ecg_data = "data : ";
+			flag = true;
+			int init_delay = 5000;
+			int end_delay = 10000;
 			starttime = millis() + init_delay;
 			endtime= millis() + end_delay;
 			Serial.print("start time: ");
@@ -46,9 +50,10 @@ class Button{
 		}
 };
 
-Button button(0);
+Button button(4);
 
 void setup() {
+	pinMode(4,INPUT_PULLUP);
 	Serial.begin(9600);
 }
 
@@ -57,14 +62,20 @@ void loop() {
 	unsigned long int Current_time= millis();
 	if (starttime < Current_time && Current_time < endtime)
 	{
+
 		int filtered_val = f.filterIn(analogRead(A0));
-		Ecg_data += (String)(filtered_val);
+		Ecg_data += (String)(filtered_val) +",";
+    
+//    count ++;/
+//		Serial.print("Count data: ");/
+//		Serial.println(count);/
 	}
 
-if ( Current_time > endtime )
-{
-	// push code
-	Serial.println(Ecg_data);
-}
-//Serial.println(Current_time);/
+	if ( Current_time > endtime && flag)
+	{
+		// push code
+		Serial.println(Ecg_data);
+		flag = false;
+	}
+	//Serial.println(Current_time);/
 }
